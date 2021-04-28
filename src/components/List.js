ee65620f53037e5da74915155842eccef7c42023
s1l3ntq <qimiller89@gmail.com>
2021-04-28 07:49:12 -0500

    Preview Player

--- src/components/List.js
+++ src/components/List.js
@@ -7,7 +7,7 @@ export class List extends Component {
     render() {
         let tracks
         if(this.props.tracks) {
-            tracks = this.props.tracks.map((t, index) => <Track key={index} name={t.name} image={t.image} artist={t.artists} />)
+            tracks = this.props.tracks.map((t, index) => <Track key={index} name={t.name} image={t.image} artist={t.artists} preview_url={t.preview_url} />)
        } else {
             tracks = "Loading..."
         }
--- src/components/Track.js
+++ src/components/Track.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react'
 
 export class Track extends Component {
     render() {
-        const {name, artist, image} = this.props
+        const {name, artist, image, preview_url} = this.props
         return (
             <div>
                 <h3> {name} </h3>
                 <p> {artist} </p>
                 <br></br>
                 <img className="thumbnail" src={image} alt={name} />
+                <img className="preview" src={preview_url} alt={name} />
+
                 
             </div>
         )
